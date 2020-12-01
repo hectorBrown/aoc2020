b4f1e8f8a7758e9482b67bf4f8eca036e3182de1
Hector Brown <hectorjbrown@gmail.com>
2020-12-01 21:42:14 +0000

    day 1

---
+++ AOC 2020/day1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  1 13:14:47 2020
+
+@author: Hector
+"""
+
+f = open("Data\day1.txt",'r')
+dat = [int(x[:-1]) for x in f.readlines()]
+for i,x in enumerate(dat[:-1]):
+    for y in dat[i + 1:]:
+        if x + y == 2020:
+            print(x*y)
+
+#%%
+f = open("Data\day1.txt",'r')
+dat = [int(x[:-1]) for x in f.readlines()]
+for i,x in enumerate(dat):
+    others = dat.copy()
+    others.pop(i)
+    for j,y in enumerate(others):
+        others_2 = others.copy()
+        others_2.pop(j)
+        for k,z in enumerate(others_2):
+            if x + y + z == 2020:
+                print(x*y*z)
