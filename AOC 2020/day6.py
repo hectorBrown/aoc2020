4c6045b98ad3439504d2a92286348049c3dcf071
hex <hectorjbrown@gmail.com>
2020-12-06 20:59:35 +0000

    day 6

---
+++ AOC 2020/day6.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  6 20:36:06 2020
+
+@author: hex
+"""
+
+
+data = ''.join(open("Data/day6.txt",'r').readlines()).translate({ord('\n'): ord(' ')})[:-1].split("  ")
+count = 0
+for group in data:
+    answered = []
+    for person in group.split(' '):
+        for question in person:
+            if not question in answered:
+                answered.append(question)
+                count += 1
+print(count)
+#%%
+data = ''.join(open("Data/day6_ex.txt",'r').readlines()).translate({ord('\n'): ord(' ')})[:-1].split("  ")
+count = 0
+for group in data:
+    answered = []
+    for person in group.split(' '):
+        answered.append(list(person))
+    count += len([all([x in y for y in answered[1:]]) for x in answered[0]])
+print(count)
